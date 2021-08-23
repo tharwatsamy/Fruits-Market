c272ccf8e5da6566f5d0466848e2c73c59b41f2c
tharwatsamy <tharwatsamy2004@gmail.com>
2021-08-23 16:23:24 +0200

    adding animation to splash view

--- lib/features/splash/presentation/widgets/splash_body.dart
+++ lib/features/splash/presentation/widgets/splash_body.dart
@@ -1,7 +1,27 @@
 import 'package:flutter/material.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({Key? key}) : super(key: key);
+
+  @override
+  _SplashViewBodyState createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  AnimationController? animationController;
+  Animation<double>? fadingAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 600));
+    fadingAnimation =
+        Tween<double>(begin: .2, end: 1).animate(animationController!);
+
+    animationController?.repeat(reverse: true);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,15 +29,18 @@ class SplashViewBody extends StatelessWidget {
       child: Column(
         children: [
           Spacer(),
-          Text(
-            'Fruit Market',
-            style: TextStyle(
-              fontFamily: 'Poppins',
-              fontSize: 51,
-              fontWeight: FontWeight.bold,
-              color: const Color(0xffffffff),
-            ),
-          ),
+         FadeTransition(
+           opacity: fadingAnimation!,
+           child: Text(
+                  'Fruit Market',
+                  style: TextStyle(
+                    fontFamily: 'Poppins',
+                    fontSize: 51,
+                    fontWeight: FontWeight.bold,
+                    color: const Color(0xffffffff),
+                  ),
+                ),
+         ),
           Image.asset('assets/images/splash_view_image.png'),
           // Adobe XD layer: 'mix_fruit_png_11' (shape)
         ],
