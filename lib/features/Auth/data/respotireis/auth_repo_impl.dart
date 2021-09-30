fb3382f0815d8177c5116db32cd4a6e2608d6420
tharwatsamy <tharwatsamy2004@gmail.com>
2021-09-30 05:43:29 +0200

    handling exceptions

--- lib/features/Auth/data/respotireis/auth_repo_impl.dart
+++ lib/features/Auth/data/respotireis/auth_repo_impl.dart
@@ -1,44 +1,51 @@
+import 'package:dartz/dartz.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'package:fruits_market/features/Auth/domain/respotries/auth_repo.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
-class AuthRepoImpl  extends AuthRepo 
-{
+class AuthRepoImpl extends AuthRepo {
   @override
-  Future completeInformation({String? name, String? phoneNumber, String? address}) {
-    
+  Future completeInformation(
+      {String? name, String? phoneNumber, String? address}) {
     throw UnimplementedError();
   }
 
   @override
-  Future loginWithFacebook() async {
-   // Trigger the sign-in flow
-  final LoginResult loginResult = await FacebookAuth.instance.login();
+  Future<Either<Exception, UserCredential>> loginWithFacebook() async {
+    try {
+      // Trigger the sign-in flow
+      final LoginResult loginResult = await FacebookAuth.instance.login();
 
-  // Create a credential from the access token
-  final OAuthCredential facebookAuthCredential = FacebookAuthProvider.credential(loginResult.accessToken!.token);
+      // Create a credential from the access token
+      final OAuthCredential facebookAuthCredential =
+          FacebookAuthProvider.credential(loginResult.accessToken!.token);
 
-  // Once signed in, return the UserCredential
-  return FirebaseAuth.instance.signInWithCredential(facebookAuthCredential);
+      // Once signed in, return the UserCredential
+      return Right(await FirebaseAuth.instance
+          .signInWithCredential(facebookAuthCredential));
+    } catch (e) {
+      return Left(Exception('someting went wrong'));
+    }
   }
 
   @override
-  Future loginWithGoogle() async  {
-  
-   
-  final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+  Future<Either<Exception, UserCredential>> loginWithGoogle() async {
+    try {
+      final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
 
+      final GoogleSignInAuthentication googleAuth =
+          await googleUser!.authentication;
 
-  final GoogleSignInAuthentication googleAuth = await googleUser!.authentication;
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
 
-
-  final credential = GoogleAuthProvider.credential(
-    accessToken: googleAuth.accessToken,
-    idToken: googleAuth.idToken,
-  );
-
-  return await FirebaseAuth.instance.signInWithCredential(credential);
+      return Right(
+          await FirebaseAuth.instance.signInWithCredential(credential));
+    } catch (e) {
+      return Left(Exception('something went wrong'));
+    }
   }
-  
 }
--- lib/features/Auth/domain/respotries/auth_repo.dart
+++ lib/features/Auth/domain/respotries/auth_repo.dart
@@ -1,7 +1,9 @@
+import 'package:dartz/dartz.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 
 abstract class AuthRepo {
-  Future loginWithGoogle();
-  Future loginWithFacebook();
+  Future<Either<Exception, UserCredential>> loginWithGoogle();
+ Future<Either<Exception, UserCredential>> loginWithFacebook();
   Future completeInformation({@required String name , @required String phoneNumber , @required String address ,});
 }
